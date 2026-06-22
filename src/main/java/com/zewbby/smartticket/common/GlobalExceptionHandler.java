65ea538e415f656dc0fdd5e5c270c244a1bec29a
Zewbbb <2423766353@qq.com>
2026-06-22 18:09:08 +0800

    记录接口未处理异常日志

--- src/main/java/com/zewbby/smartticket/common/GlobalExceptionHandler.java
+++ src/main/java/com/zewbby/smartticket/common/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.zewbby.smartticket.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BusinessException.class)
     public ApiResponse<Void> handleBusinessException(BusinessException exception) {
@@ -21,6 +25,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ApiResponse<Void> handleException(Exception exception) {
+        LOGGER.error("Unhandled API exception", exception);
         return ApiResponse.fail(500, "系统错误");
     }
 }
